a16903f4c5091e5750ec6df739ed49d510fb9b86
SAM082024 <smia082024@gmail.com>
2024-08-26 14:06:14 +0200

    array trimmer

--- main.js
+++ main.js
@@ -75,8 +75,31 @@ The function should return:
 
 ******************************************************************************/
 
-export const greeter = () => {
-  //your code here
+export const greeter = (name, hour) => {
+  if (hour < 0)
+  {
+    return "Invalid time";
+  }
+  else if (hour >= 0 && hour <= 5)
+  {
+    return `Good night ${name}`
+  }
+  else if (hour >= 6 && hour <= 11)
+  {
+    return `Good morning ${name}`
+  }
+  else if (hour >= 12 && hour <= 17)
+  {
+    return `Good day ${name}`
+  }
+  else if (hour >= 18 && hour <= 23)
+  {
+    return `Good evening ${name}`
+  }
+  else if (hour > 23)
+  {
+    return "Invalid time";
+  }
 };
 
 /******************************************************************************
@@ -93,8 +116,13 @@ Example 2: ["One", "Two", "Three", "Four", "Five", "Six"] should return
 ["Two", "Three", "Four", "Five"]
 ******************************************************************************/
 
-export function arrayTrimmer() {
-  //your code here
+export function arrayTrimmer(arr) {
+  if (arr.length <=2)
+  {
+    return[];
+  }
+    return arr.slice(1, -1);
+  
 }
 
 /******************************************************************************
